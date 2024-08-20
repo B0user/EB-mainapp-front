42732e619bb929ce4274c6fbcaaab96012ef882c
ArmanJumashev <124795505+ArmanJumashev@users.noreply.github.com>
2024-08-20 17:27:34 +0500

    new design mobile 2

--- src/features/landing/LandingHeroBlock2.js
+++ src/features/landing/LandingHeroBlock2.js
@@ -12,9 +12,7 @@ const LandingBlock = () => {
                 <div className="landing-text">
                     Ваш гид в запутанном мире образования
                 </div>
-                <a href="#problems">
-                <button  style={{height: '50px', borderRadius: '25px', width: '200px', border: '0', marginTop: '50px', letterSpacing: '0.4rem', fontSize:'17px'}}>ДАЛЕЕ</button>
-                </a>
+
                 </div>
         </div>
     );
