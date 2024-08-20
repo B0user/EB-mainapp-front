e7b7d71cc85a3f53a4f8e9e0f98c78e1e16ecbb3
ArmanJumashev <124795505+ArmanJumashev@users.noreply.github.com>
2024-08-20 16:09:36 +0500

    new design mobile

---
+++ src/features/landing/LandingHeroBlock2.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Landing.css";
+
+const LandingBlock = () => {
+    return (
+        <div className="landing-block">
+            <div className="landing-image-container">
+                <img src="edubridge-preview.png" alt="Guiding Education" className="landing-image" />
+                <div className="landing-shadow"></div>
+            </div>
+            <div className="landing-text-container">
+                <div className="landing-text">
+                    Ваш гид в запутанном мире образования
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LandingBlock;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from '../features/landing/LandingHeader'
 import LandingHeroBanner from '../features/landing/LandingHeroBanner'
 import LandingBodyNews from "../features/landing/LandingBodyNews"
@@ -6,17 +6,53 @@ import LandingResourcesSection from "../features/landing/LandingResourcesSection
 import LandingFooter from "../features/landing/LandingFooter"
 import LandingVideoSection from "../features/landing/LandingVideoSection";
 import LandingSocialSection from "../features/landing/LandingSocialSection";
+import LandingHeroBlock2 from "../features/landing/LandingHeroBlock2";
+import ScrollProgress from "../features/landing/ScrollProgress";
+import LandingProblems from "../features/landing/LandingProblems";
+import LandingCons from "../features/landing/LandingCons";
+import LandingSolution from "../features/landing/LandingSolution";
+import LandingLocation from "../features/landing/LandingLocation";
 
 const LandingPage = () => {
-  return(
+    const [showScrollProgress, setShowScrollProgress] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const secondBlock = document.getElementById("secondBlock");
+
+            if (secondBlock) {
+                const secondBlockTop = secondBlock.offsetTop; // Изменено на offsetTop
+                const scrollPosition = window.scrollY + window.innerHeight;
+
+                if (scrollPosition >= secondBlockTop + window.innerHeight / 2) {
+                    setShowScrollProgress(true);
+                } else {
+                    setShowScrollProgress(false);
+                }
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        // Вызов handleScroll после монтирования компонента
+        handleScroll();
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+
+
+    return(
       <>
       <Header />
-      <LandingHeroBanner />
-      <LandingBodyNews />
-      <LandingResourcesSection />
-      <LandingVideoSection />
-          <LandingSocialSection />
-          <LandingFooter />
+          {showScrollProgress && <ScrollProgress />}
+      <LandingHeroBlock2 id="secondBlock"/>
+      <LandingProblems />
+      <LandingCons />
+      <LandingSolution />
+      <LandingLocation />
       </>
   );
 }
